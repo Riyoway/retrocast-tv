53124c7309683731812dfa1efa6c4e3f80989786
riyoway <ryohsg@gmail.com>
2026-07-18 00:03:38 +0900

    Use optimized app icon assets

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,22 @@
-const CACHE = 'retrocast-shell-v1'
-const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg']
+const CACHE = 'retrocast-shell-v2'
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './favicon.ico',
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './favicon-48x48.png',
+  './apple-touch-icon.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-192.png',
+  './icons/maskable-512.png',
+]
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL))))
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()))
+self.addEventListener('activate', (event) => event.waitUntil(
+  caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()),
+))
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) return
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
